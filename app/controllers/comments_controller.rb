3c97b8c422eb718d317eeb4edebe41817cdbbb03
Scuatsh <barucvilla@gmail.com>
2015-03-24 16:56:17 -0400

    Update comments_controller.rb
    se le agrego .permit y esto permite superar el error pero no insertar correctamente el comentario, ademas se le agrego parametros que se requiere por defecto

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,17 @@
 class CommentsController < ApplicationController
   def create
   	@post = Post.find(params[:post_id])
-  	@comment = @post.comments.build(params[:comment])
+  	@comment = @post.comments.build(params.permit[:comment])
   	@comment.save
   	redirect_to @posts
   end
 
   def destroy
   end
+   private
+
+  def comment_params
+      params.require(:comment).permit(:text)
+   end
+
 end
